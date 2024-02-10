f7060d5cf9ff0503154d8ff9a634612bee55fcb4
Deep Tejaswi Adireddy <119724015+DeepTejaswi315@users.noreply.github.com>
2024-02-10 16:45:55 +0530

    Create math operations.cpp

---
+++ math operations.cpp
@@ -0,0 +1,11 @@
+math operations.cpp#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for(int i=a+1;i<b;i++)
+    {
+        cout<<i<<" "<<i*i<<" "<<i*i*i<<endl;
+    }
+}
